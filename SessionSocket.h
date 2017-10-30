3fdfe8b36272b7e5f92c96dfdac53988908f8281
tanghc10 <738433477@qq.com>
2017-10-30 20:22:18 +0800

    BugFix

--- SessionSocket.h
+++ SessionSocket.h
@@ -11,7 +11,9 @@ public:
 public:
 	BOOL LogoIn(LPSTR lpBuff, int nlen, char from_user[20]);
 	BOOL SendMSG(LPSTR lpBuff, int nlen, char to_user[20], char from_user[20]);
+	BOOL Is_Connect;
 	CString m_strUserName;
+	virtual void OnClose(int nErrorCode);
 };
 
 
